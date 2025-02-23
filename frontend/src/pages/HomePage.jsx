0234921d6309cd872074ecd6b1f84f2f4d29ece7
Jerónimo Lucero <jeronimoluceror@gmail.com>
2025-02-23 11:08:57 -0300

    small fix

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import background from 'img/background.jpeg'; // Ajusta la ruta según tu estructura de carpetas
+import background from '../../public/img/background.jpeg'; // Ajusta la ruta según tu estructura de carpetas
 
 const HomePage = () => {
   return (
